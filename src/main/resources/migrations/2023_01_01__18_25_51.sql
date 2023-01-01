1f234a03d6eafeb6f02c1e73e80db241b4f6e581
Czaro <czareklutrzykowski@gmail.com>
2023-01-01 18:48:11 +0100

    konfiguracja liquibase i bazy danych

--- src/main/resources/migrations/2023_01_01__18_25_51.sql
+++ src/main/resources/migrations/2023_01_01__18_25_51.sql
@@ -1,4 +1,13 @@
 -- liquibase formatted sql
--- changeset shop:1.0
+-- changeset clutrzykowski:1.0
 -- validCheckSum: ANY
 
+create table product
+(
+    id          bigint        not null auto_increment PRIMARY KEY,
+    name        varchar(255)  not null,
+    category    varchar(255)  not null,
+    description text          not null,
+    price       decimal(9, 2) not null,
+    currency    varchar(3)    not null
+);
