e12785175ef31f8c83c037b3fc074c253620eb64
Czaro <czareklutrzykowski@gmail.com>
2023-02-11 22:08:07 +0100

    zliczanie produktów

--- src/main/java/com/shop/cart/controller/CartItemController.java
+++ src/main/java/com/shop/cart/controller/CartItemController.java
@@ -2,10 +2,7 @@ package com.shop.cart.controller;
 
 import com.shop.cart.service.CartItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("cartItems")
@@ -15,7 +12,12 @@ public class CartItemController {
     private final CartItemService cartItemService;
 
     @DeleteMapping("/{id}")
-    public void deleteCartItem(@PathVariable Long id){
+    public void deleteCartItem(@PathVariable Long id) {
         cartItemService.delete(id);
+    }
+
+    @GetMapping("/count/{cartId}")
+    public Long countItemInCart(@PathVariable Long cartId) {
+        return cartItemService.countItemInCart(cartId);
     }
 }
